296a6a317f1fd664fb8fa762a2336b8f3d5a6158
yang-shuohao <2499134263@qq.com>
2024-07-12 09:36:40 +0800

    添加网络部分

---
+++ MiniFrame/Network/Server/Server/Server/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!DbManager.Connect("game", "127.0.0.1", 3306, "root", ""))
+            {
+                return;
+            }
+
+            DbManager.CreatePlayer("abc123");
+            PlayerData pd = DbManager.GetPlayerData("abc123");
+            pd.coin = 256;
+            DbManager.UpdatePlayerData("abc123", pd);
+
+            NetManager.StartLoop(8888);
+        }
+    }
+}
